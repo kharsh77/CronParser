b875013720b6cd6528b503e6761fa6fc13f7f3e6
Harsh kumar <13298419+kharsh77@users.noreply.github.com>
2024-04-07 11:47:18 +0545

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import sys
 if __name__=='__main__':
     input_string = sys.argv[1]
     e = ExpressionParser(input_string)
-    from pprint import pprint
-    pprint(e.expand())
     e.print_values()
 
 
