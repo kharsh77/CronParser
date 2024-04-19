260f7b3214a88a9bd981ae474068b9f96a69e9e4
Harsh <harsh77@Harshs-MacBook-Pro.local>
2024-04-19 11:19:44 +0530

    deleted all files

--- components/command_component.py
+++
@@ -1,6 +0,0 @@
-
-class CommandComponent:
-    def __init__(self, current_expression):
-        self.current_values = [current_expression]
-    def get_values(self):
-        return ["Command", self.current_values]
